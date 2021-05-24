5fb1c667ce30ba9186ffda9939725e73622ce2bf
Oskar Owca <71120011+Kriperos@users.noreply.github.com>
2021-05-24 16:28:43 +0100

    Add files via upload

---
+++ praca na 6/main.js
@@ -0,0 +1,19 @@
+const beg = document.createElement('div')
+beg.setAttribute('class','wrapper')
+app.appendChild(beg)
+
+const be2 = document.createElement('div')
+be2.setAttribute('class','news-item hero-item')
+beg.appendChild(be2)
+
+const be3 = document.createElement('div')
+be3.setAttribute('class','news-item standard-item')
+beg.appendChild(be3)
+
+const b4 = document.createElement('div')
+b4.setAttribute('class','news-item standard-item')
+beg.appendChild(b4)
+
+const b5 = document.createElement('div')
+b5.setAttribute('class','news-item standard-item')
+beg.appendChild(b5)
